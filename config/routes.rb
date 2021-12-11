aff3665d8da6975b9d9ccdddc700ade4be2596bd
COOLIRON2311 <ignatenko.ivan.23112000@gmail.com>
2021-12-11 17:54:55 +0300

    Refactored users, tasks and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
-  post '/auth/sign_up', to: 'auth#sign_up'
-  post 'auth/sign_in', to: 'auth#sign_up'
+  resource :user do
+    post '/user/sign_up', to: 'users#sign_up'
+    post '/user/sign_in', to: 'users#sign_in'
+  end
 
-  post 'task/create', to: 'task#create'
-  get 'task/result', to: 'task#result'
-  get '/stats', to: 'stats#get'
+  resource :task do
+    post '/task/create', to: 'tasks#create'
+    get '/task/result', to: 'tasks#result'
+  end
+
+  resource :stat do
+    get '/stats', to: 'stats#get'
+  end
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
